8cf11142f5bcf151a5304ab0721279e7d980be5a
Hrabi <hrabi.ahmed88@gmail.com>
2020-10-13 01:24:53 +0100

    before format

--- authenticate.js
+++ authenticate.js
@@ -4,6 +4,7 @@ var User = require('./models/user');
 var JwtStrategy = require('passport-jwt').Strategy;
 var ExtractJwt = require('passport-jwt').ExtractJwt;
 var jwt = require('jsonwebtoken');
+var FacebookTokenStrategy = require('passport-facebook-token');
 
 var config = require('./config.js');
 
@@ -13,7 +14,7 @@ passport.deserializeUser(User.deserializeUser());
 
 exports.getToken = function(user) {
     return jwt.sign(user, config.secretKey,
-        {expiresIn: 3600});
+        {expiresIn: 7600});
 };
 
 var opts = {};
@@ -49,5 +50,31 @@ exports.verifyAdmin = function(req,res,next){
         next();
     }   
   };
+  exports.facebookPassport = passport.use(new FacebookTokenStrategy({
+      clientID:config.facebook.clientId,
+      clientSecret: config.facebook.clientSecret
+    },(accessToken, refreshToken,profile,done)=>{
+        User.findOne({facebookId: profile.id},(err,user)=>{
+            if(err){
+                return done(err,false);
+            }
+            if(!err && user != null){
+                return done(null,user);
+            }
+            else {
+                user = new User({username: profile.displayName});
+                user.facebookId = profile.id;
+                user.firstname = profile.name.givenName;
+                user.lastname = profile.name.familyName;
+                user.save((err,user)=>{
+                    if(err)
+                        return done(err,false);
+                    else   
+                        return done(null, user);
+                });
+            }
+        });
+    }
+  ));
 
 
